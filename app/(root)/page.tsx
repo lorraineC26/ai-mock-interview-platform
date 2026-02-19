eccd114bc36e9cff248c0101138ad5332df1520e
lorraineC26 <lorraine.quinyee@gmail.com>
2026-02-19 14:26:05 -0500

    fixed the layout issue for homepg

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -30,7 +30,7 @@ const page = () => {
       <section className="flex flex-col gap-6 mt-8">
         <h2>Your Interviews</h2>
 
-        <div className="interview-section">
+        <div className="interviews-section">
           {dummyInterviews.map((interview) => (
             <InterviewCard {...interview} key={interview.id} />
           ))}
