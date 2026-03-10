1f0128bbaaab4e94142aaaa800f6e1fbf5e84536
lorraineC26 <lorraine.quinyee@gmail.com>
2026-03-10 16:12:32 -0400

    feat: create sign-in logic on client side

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -12,9 +12,9 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "@/components/FormField";
 import { useRouter } from "next/navigation";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/firebase/client";
-import { signUp } from "@/lib/actions/auth.action";
+import { signUp, signIn } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -64,6 +64,22 @@ const AuthForm = ({ type }: { type: FormType }) => {
         toast.success("Account created successfully!");
         router.push("/sign-in");
       } else {
+        // Sign-in logic (handled by Firebase Authentication)
+        const { email, password } = values;
+
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        const idToken = await userCredential.user.getIdToken();
+
+        if(!idToken) {
+          toast.error('Sign in failed')
+          return;
+        }
+
+        await signIn({
+          email, idToken
+        })
+
         toast.success("Signed in successfully!");
         router.push("/");
       }
