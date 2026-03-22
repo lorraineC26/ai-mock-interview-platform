650a61547cc7594442c909eb82e288568f1fedf9
lorraineC26 <lorraine.quinyee@gmail.com>
2026-03-22 17:31:03 -0400

    feat: Create interview page layout and integrate user data

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -1,13 +1,53 @@
+import Agent from "@/components/Agent";
+import DisplayTechIcons from "@/components/DisplayTechIcons";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import { getInterviewById } from "@/lib/actions/general.action";
+import { getRandomInterviewCover } from "@/lib/utils";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 const page = async ({ params }: RouteParams) => {
   const { id } = await params;
   const interview = await getInterviewById(id);
+  const user = await getCurrentUser();
 
   if (!interview) redirect("/");
 
-  return <div>page</div>;
+  return (
+    <>
+      <div className="flex flex-row gap-4 justify-between">
+        <div className="flex flex-row gap-4 items-center max-sm:flex-col">
+          <div className="flex flex-row gap-4 items-center">
+            <Image
+              src={getRandomInterviewCover()}
+              alt="Interview Cover"
+              width={40}
+              height={40}
+              className="rounded-full object-cover size-10"
+            />
+            {/* title */}
+            <h3 className="capitalize">{interview.role}</h3>
+          </div>
+
+          {/* Tech Icons */}
+          <DisplayTechIcons techStack={interview.techstack} />
+        </div>
+
+        <p className="bg-dark-200 px-4 py-2 rounded-lg h-fit capitalize">
+          {interview.type}
+        </p>
+      </div>
+
+      {/* Agent Component: conduct with previous generated Qs */}
+      <Agent
+        userName={user?.name!}
+        userId={user?.id}
+        interviewId={id}
+        type="interview"
+        questions={interview.questions}
+      />
+    </>
+  );
 };
 
 export default page;
