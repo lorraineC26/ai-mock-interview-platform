b63e8745496df0b2dab82bd5a14970c0ed624a8e
lorraineC26 <lorraine.quinyee@gmail.com>
2026-03-22 18:53:14 -0400

    feat: implement feedback generation logic in createFeedback function; migrate from generateObject (deprecated) to generateText for AI analysis

--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,5 +1,8 @@
 "use server";
+import { feedbackSchema } from "@/constants";
 import { db } from "@/firebase/admin";
+import { generateText, Output } from "ai";
+import { google } from "@ai-sdk/google";
 
 //fetch all the interviews of a specific user
 export async function getInterviewsByUserId(
@@ -42,4 +45,40 @@ export async function getInterviewById(id: string): Promise<Interview | null> {
   const interview = await db.collection("interviews").doc(id).get();
 
   return (interview.data() as Interview) || null;
+}
+
+export async function createFeedback(params: CreateFeedbackParams) {
+  const { interviewId, userId, transcript } = params;
+
+  try {
+    const formattedTranscript = transcript
+      .map(
+        (sentence: { role: string; content: string }) =>
+          `- ${sentence.role}: ${sentence.content}\n`,
+      )
+      .join("");
+
+    // migrate from generateObject to generateText due to deprecation
+    // https://ai-sdk.dev/docs/migration-guides/migration-guide-6-0#generateobject-and-streamobject-deprecation
+    const { output } = await generateText({
+      model: google("gemini-2.5-flash-lite"),
+      output: Output.object({ schema: feedbackSchema }),
+      prompt: `
+        You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+        Transcript:
+        ${formattedTranscript}
+
+        Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+        - **Communication Skills**: Clarity, articulation, structured responses.
+        - **Technical Knowledge**: Understanding of key concepts for the role.
+        - **Problem-Solving**: Ability to analyze problems and propose solutions.
+        - **Cultural & Role Fit**: Alignment with company values and job role.
+        - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+        `,
+      system:
+        "You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories",
+    });
+  } catch (error) {
+    console.error("Error saving feedback:", error);
+  }
 }
